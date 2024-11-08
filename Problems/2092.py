db558ac84c465b00a83ba8041c316b2eceab63f2
Yan Werlley <yanwerlley0101@gmail.com>
2024-11-07 21:49:58 -0300

    feat(2092): implements algorithm to find people who know the secret using Union-Find
    This update optimizes the solution by implementing Union-Find, thus making it pass, but in the long run, it needs improvements.
    
    Co-authored-by: SamuelGSouza <samuelgomes318@gmail.com>

--- Problems/2092.py
+++ Problems/2092.py
@@ -1,29 +1,52 @@
+from collections import defaultdict
+
 class Solution:
     def findAllPeople(self, n, meetings, firstPerson):
-        knows_secret = set([0, firstPerson])
+        # Inicializar as pessoas que sabem o segredo
+        knows_secret = {0, firstPerson}
         
+        # Agrupar reuniões por tempo e ordenar
         meetings.sort(key=lambda x: x[2])
         
-        for x, y, time in meetings:
-            if x in knows_secret or y in knows_secret:
-                knows_secret.add(x)
-                knows_secret.add(y)
-                
-                queue = deque([x, y])
-                visited = set([x, y])
-                
-                while queue:
-                    person = queue.popleft()
-                    for a, b, t in meetings:
-                        if t != time:
-                            continue
-                        if a == person and b not in visited:
-                            visited.add(b)
-                            knows_secret.add(b)
-                            queue.append(b)
-                        elif b == person and a not in visited:
-                            visited.add(a)
-                            knows_secret.add(a)
-                            queue.append(a)
+        i = 0
+        while i < len(meetings):
+            time = meetings[i][2]
+            group = defaultdict(list)
+            j = i
+            
+            # Agrupar todas as reuniões no mesmo tempo
+            while j < len(meetings) and meetings[j][2] == time:
+                x, y, _ = meetings[j]
+                group[x].append(y)
+                group[y].append(x)
+                j += 1
+            
+            # Usar Union Find para determinar os grupos que sabem o segredo
+            parent = {p: p for p in group}
+            
+            def find(x):
+                if parent[x] != x:
+                    parent[x] = find(parent[x])
+                return parent[x]
+            
+            def union(x, y):
+                parent[find(x)] = find(y)
+            
+            # Conectar todos os participantes
+            for p1 in group:
+                for p2 in group[p1]:
+                    union(p1, p2)
+            
+            # Propagar o segredo
+            secret_group = set()
+            for p in group:
+                if p in knows_secret:
+                    secret_group.add(find(p))
+            
+            for p in group:
+                if find(p) in secret_group:
+                    knows_secret.add(p)
+            
+            i = j
         
         return list(knows_secret)
